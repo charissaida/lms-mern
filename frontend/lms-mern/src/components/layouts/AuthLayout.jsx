f6cbbb0789ff6dde987ff8115031e33436eccb04
charissaida <charis.saida@gmail.com>
2025-06-04 00:19:52 +0700

    update frontend

--- frontend/lms-mern/src/components/layouts/AuthLayout.jsx
+++ frontend/lms-mern/src/components/layouts/AuthLayout.jsx
@@ -4,8 +4,8 @@ import UI_IMG from "../../assets/images/auth-img.png";
 const AuthLayout = ({ children }) => {
   return (
     <div className="flex">
-      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">{children}</h2>
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12 flex items-center justify-center">
+        <h2 className="w-full text-lg font-medium text-black">{children}</h2>
       </div>
       <div className="hidden md:flex h-screen items-center justify-center bg-blue-50 bg-cover bg-no-repeat bg-center overflow-hidden">
         <img src={UI_IMG} alt="" className="" />
--- frontend/lms-mern/src/pages/Auth/Login.jsx
+++ frontend/lms-mern/src/pages/Auth/Login.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import { UserContext } from "../../context/userContext";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  const { updateUser } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
@@ -38,6 +40,7 @@ const Login = () => {
 
       if (token) {
         localStorage.setItem("token", token);
+        updateUser(response.data);
 
         // Rediret based on role
         if (role === "admin") {
@@ -57,8 +60,7 @@ const Login = () => {
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-        <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter your details to login</p>
+        <h3 className="text-xl text-center font-semibold text-black mb-6">Sign in to your account</h3>
 
         <form onSubmit={handleLogin}>
           <Input value={email} onChange={({ target }) => setEmail(target.value)} label="Email Address" placeholder="email@example.com" type="text" />
@@ -66,8 +68,8 @@ const Login = () => {
 
           {error && <p className="text-xs pb-2.5 text-red-500">{error}</p>}
 
-          <button type="submit" className="btn-primary">
-            Login
+          <button type="submit" className="btn-gray">
+            Sign In
           </button>
 
           <p className="text-[13px] text-slate-800 mt-3">
