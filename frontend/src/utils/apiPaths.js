60b5b53f612e1094f0a12ac362aafd7bc345eff2
charissaida <charis.saida@gmail.com>
2025-07-12 11:29:20 +0700

    project deploy

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/api";
 
 export const API_PATHS = {
   AUTH: {
